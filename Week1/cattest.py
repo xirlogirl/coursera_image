7e443bb879bcbb3a1376df1fc6b573e97eb7fd32
Anudeep Kanwar <anudeep.kanwar@gmail.com>
2017-09-02 16:45:22 -0400

    fixed some egregious style

--- Week1/cattest.py
+++ Week1/cattest.py
@@ -1,9 +1,17 @@
+'''
+
+Simple display example using scipy and matplotlib
+
+'''
+import matplotlib.pyplot as plt
 from scipy import ndimage
 from scipy import misc
+
+IMAGE = '/Users/akanwar/Documents/Image Processing course/assignments/cat.jpg'
+
 #l = misc.lena()
 #misc.imsave('lena.png', l) # uses the Image module (PIL)
-l = misc.imread('/Users/akanwar/Documents/Image Processing course/assignments/cat.jpg')
-import matplotlib.pyplot as plt
+l = misc.imread(IMAGE)
 l[1:199, 1:230] = 256
 plt.imshow(l)
 plt.show()
